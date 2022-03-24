a429b6382600a8b3ed224ebfef2b37c883b77ab1
diptimayeetarei <tareidiptimayee@gmail.com>
2022-03-24 05:41:50 -0800

    Update wdio.conf.js

--- wdio.conf.js
+++ wdio.conf.js
@@ -320,19 +320,22 @@ exports.config = {
    * @param {<Object>} results object containing test results
    */
   onComplete: function () {
-    // const reportError = new Error("Could not generate Allure report");
-    // const generation = allure(["generate", "allure-results", "--clean"]);
-    // return new Promise((resolve, reject) => {
-    //   const generationTimeout = setTimeout(() => reject(reportError), 5000);
-    //   generation.on("exit", function (exitCode) {
-    //     clearTimeout(generationTimeout);
-    //     if (exitCode !== 0) {
-    //       return reject(reportError);
-    //     }
-    //     console.log("Allure report successfully generated");
-    //     resolve();
-    //   });
-    // });
+    const reportError = new Error("Could not generate Allure report");
+    const generation = allure(["generate", "allure-results", "--clean"]);
+    return new Promise((resolve, reject) => {
+      const generationTimeout = setTimeout(() => reject(reportError), 5000);
+
+      generation.on("exit", function (exitCode) {
+        clearTimeout(generationTimeout);
+
+        if (exitCode !== 0) {
+          return reject(reportError);
+        }
+
+        console.log("Allure report successfully generated");
+        resolve();
+      });
+    });
   },
 
   /**
